bef7ae3b475fd0b9abd63822263daaeffa179489
aniruddhakhan <aniruddhakhan747@gmail.com>
2024-02-28 00:28:59 -0700

    Updated read me.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,14 +2,9 @@ import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
     return (
-      <>
       <div className="p-4">
         <UserButton afterSignOutUrl="/" />
       </div>
-      <div>
-        <h1 className="right-44">Hello my name is aniruddha</h1>
-      </div>
-      </>
     );
   }
 
