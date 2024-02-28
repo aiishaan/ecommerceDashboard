d8ea172930df2ea85fb2419e38faefc86d50e449
aniruddhakhan <aniruddhakhan747@gmail.com>
2024-02-28 00:18:29 -0700

    updated page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,14 @@ import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
     return (
+      <>
       <div className="p-4">
         <UserButton afterSignOutUrl="/" />
       </div>
+      <div>
+        <h1 className="right-44">Hello</h1>
+      </div>
+      </>
     );
   }
 
